3f396ce4108b38b755f732c8c5bbc3bcef83a910
pengchenggang <76526211@qq.com>
2020-01-06 14:33:29 +0800

    db.json && index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+module.exports = () => {
+  const data = {
+    "select": [
+      {
+        "code": "N1",
+        "codeName": "N1名字"
+      },
+      {
+        "code": "N2",
+        "codeName": "N2名字"
+      }
+    ]
+  }
+  return data
+}
